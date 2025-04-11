a756dcb87fd87a4412803deac12f492e503ae83a
Danhho1 <22211tt2553@mail.tdc.edu.vn>
2025-04-11 23:16:44 +0700

    new

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name',100);
+            $table->string('name', 100);
+            
+            $table->string('phone', 15);
+            
+            $table->string('address', 255);
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
